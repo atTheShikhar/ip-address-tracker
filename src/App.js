52a67c1c2908c7a7733c37cc85165de6c8036218
Shikhar Singh <shikharsingh.original@gmail.com>
2022-08-13 19:21:08 +0530

    fix: change ip provider api

--- src/App.js
+++ src/App.js
@@ -1,59 +1,62 @@
-import React,{useEffect,useState} from 'react'
-import SearchBar from './components/SearchBar'
-import "./App.css"
-import InfoBox from './components/InfoBox'
-import RenderMap from './components/RenderMap'
-import LoadingBox from './components/LoadingBox'
+import React, { useEffect, useState } from "react";
+import SearchBar from "./components/SearchBar";
+import "./App.css";
+import InfoBox from "./components/InfoBox";
+import RenderMap from "./components/RenderMap";
+import LoadingBox from "./components/LoadingBox";
 
 function App() {
-  const [locationData, setLocationData] = useState(null) 
-  const [ipAddress,setIpAddress] = useState(''); 
+  const [locationData, setLocationData] = useState(null);
+  const [ipAddress, setIpAddress] = useState("");
   //Set this state if ip address entered is invalid
-  const [invalidIp,setInvalidIp] = useState(false); 
+  const [invalidIp, setInvalidIp] = useState(false);
 
-  const apiEndpoint = `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=${ipAddress}`;
-  
+  // const apiEndpoint = `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=${ipAddress}`;
+  const apiEndpoint = `http://ip-api.com/json/${ipAddress}?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query`;
+
   async function fetchData() {
-    //Set to null so that to show loading screen 
+    //Set to null so that to show loading screen
     setLocationData(null);
     const res = await fetch(apiEndpoint);
     const data = await res.json();
-    if(data.code === 422) {
+    if (data.status === "fail") {
       console.log(data.messages);
       //Set Ip address to null so that page redirects to clients location
-      setIpAddress('');
+      setIpAddress("");
 
       //Set invalidIp to true
       setInvalidIp(true);
       //After 3 seconds change invalidIp to false
-      setTimeout(() => {setInvalidIp(false)},3000)
+      setTimeout(() => {
+        setInvalidIp(false);
+      }, 3000);
     } else {
       setLocationData(data);
     }
   }
 
-  useEffect(() => { 
+  useEffect(() => {
     fetchData();
-  },[ipAddress])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ipAddress]);
 
   return (
     <div className="main">
-
-      {invalidIp ? (<div className="error">Invalid IP !</div>) : (<div></div>)}
+      {invalidIp ? <div className="error">Invalid IP !</div> : <div></div>}
 
       <div className="top">
-        <div className="header">
-          IP Address Tracker
-        </div>
-        <SearchBar setIpAddress={setIpAddress}/>
+        <div className="header">IP Address Tracker</div>
+        <SearchBar setIpAddress={setIpAddress} />
         {!locationData && <LoadingBox />}
-        {locationData && <InfoBox data={locationData}/>}
+        {locationData && <InfoBox data={locationData} />}
       </div>
 
-      {!locationData && (<div className="bottom loading">Loading...</div>)}
-      {locationData && <RenderMap lat={locationData.location.lat} lng={locationData.location.lng}/>} 
+      {!locationData && <div className="bottom loading">Loading...</div>}
+      {locationData && (
+        <RenderMap lat={locationData.lat} lng={locationData.lon} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function InfoBox(props) {
-    const {data} = props;
+  const { data } = props;
 
-    return (
-        <div className="middle">
-            <div className="ip wrapper">
-                <div className="name">IP ADDRESS</div>
-                <div className="value">{data.ip}</div>
-            </div>
+  return (
+    <div className="middle">
+      <div className="ip wrapper">
+        <div className="name">IP ADDRESS</div>
+        <div className="value">{data.query}</div>
+      </div>
 
-            <div className="location wrapper">
-                <div className="name">LOCATION</div>
-                <div className="value">{data.location.city+','+data.location.region+" "+data.location.postalCode}</div>
-            </div>
+      <div className="location wrapper">
+        <div className="name">LOCATION</div>
+        <div className="value">
+          {data.city + "," + data.region + " " + data.zip}
+        </div>
+      </div>
 
-            <div className="timezone wrapper">
-                <div className="name">TIMEZONE</div>
-                <div className="value">UTC {data.location.timezone}</div>
-            </div>
+      <div className="timezone wrapper">
+        <div className="name">TIMEZONE</div>
+        <div className="value">{data.timezone}</div>
+      </div>
 
-            <div className="isp wrapper">
-                <div className="name">ISP</div>
-                <div className="value">{data.isp}</div>
-            </div>
-        </div>        
-    )
+      <div className="isp wrapper">
+        <div className="name">ISP</div>
+        <div className="value">{data.isp}</div>
+      </div>
+    </div>
+  );
 }
 
-export default InfoBox 
+export default InfoBox;
